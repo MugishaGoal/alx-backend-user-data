23d243fdd7aeecc54a136a65f4b3b28451632947
Mugisha Goal <degaullemugisha@gmail.com>
2024-04-13 23:44:28 +0200

    A function that returns a log message obfuscated

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,6 +2,7 @@
 """A function that returns the log message obfuscated"""
 
 
+import logging
 import re
 
 
@@ -10,3 +11,19 @@ def filter_datum(fields, redaction, message, separator):
     for field in fields:
         message = re.sub(fr'(?<={field}=)[^{separator}]+', redaction, message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+
+    def __init__(self, fields):
+        super().__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """ Format the log record, redacting specified fields """
+        log_message = super().format(record)
+        return filter_datum(self.fields, self.REDACTION, log_message, self.SEPARATOR)
