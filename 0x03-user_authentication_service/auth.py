98f0d4607d57b6d92f10acea5284c24a27056bfe
Mugisha Goal <degaullemmugisha@gmail.com>
2024-04-25 16:43:23 +0000

    AUth module

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -38,3 +38,12 @@ class Auth:
             # Add the user to the database
             self._db.add_user(email=email, hashed_password=hashed_password)
             return user
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """Validates login credentials"""
+        try:
+            user = self._db.find_user_by(email=email)
+            return bcrypt.checkpw(password.encode('utf-8'),
+                    user.hashed_password.encode('utf-8'))
+        except NoResultFound:
+            return False
