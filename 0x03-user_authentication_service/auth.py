9be95c0f3d213e08aa298c7bb9d366bb88bb1810
Mugisha Goal <degaullemmugisha@gmail.com>
2024-04-25 15:19:58 +0000

    A method that takes in a password string arguments and returns bytes

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -4,9 +4,36 @@
 
 import bcrypt
 
+from db import DB
+from user import User
+
 
 def _hash_password(password: str) -> bytes:
     """A method that takes in a password string arguments and returns bytes"""
     salt = bcrypt.gensalt()
     hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
     return hashed_password
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """Register a new user"""
+        try:
+            # Check if user already exists with the provided email
+            self._db.find_user_by(email=email)
+            # If user exists, raise ValueError
+            raise ValueError(f"User {email} already exists")
+        except NoResultFound:
+            # If user does not exist, hash the password
+            hashed_password = _hash_password(password)
+            # Create a new user object
+            user = User(email=email, hashed_password=hashed_password)
+            # Add the user to the database
+            self._db.add_user(user)
+            return user
+
