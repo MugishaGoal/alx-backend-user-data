22a751db0d676c2406ed2e051d3d9266c89a41d6
Mugisha Goal <degaullemmugisha@gmail.com>
2024-04-25 17:25:45 +0000

    Auth module

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -17,6 +17,10 @@ def _hash_password(password: str) -> bytes:
     hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
     return hashed_password
 
+def _generate_uuid() -> str:
+    """Generates a UUID"""
+    return str(uuid4())
+
 
 class Auth:
     """Auth class to interact with the authentication database.
