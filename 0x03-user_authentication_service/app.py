533224745371bd89859c05299fded0f4665fc25c
Mugisha Goal <degaullemmugisha@gmail.com>
2024-04-25 16:17:36 +0000

    A Flask app

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -24,7 +24,7 @@ def users() -> str:
     try:
         user = AUTH.register_user(email, password)
         return jsonify({"email": email, "message": "user created"}), 200
-    except ValueError as e:
+    except ValueError:
         return jsonify({"message": "email already registered"}), 400
 
 
