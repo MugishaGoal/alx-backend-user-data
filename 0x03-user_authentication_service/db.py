a501ce71831235c6e47b9e0a2586dfac5700a0a7
Mugisha Goal <degaullemmugisha@gmail.com>
2024-04-25 14:30:24 +0000

    DB module

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -39,7 +39,7 @@ class DB:
         self._session.commit()
         return user
 
-    def filter_user_by(self, **kwargs) -> User:
+    def find_user_by(self, **kwargs) -> User:
         """Find a user in the database by the given keyword arguments"""
         try:
             user = self._session.query(User).filter_by(**kwargs).first()
